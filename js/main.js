6e398e8429bd9b5d295cf4c2cb69ed5100785dca
Evan Emenegger <evanemenegger@gmail.com>
2017-10-10 17:58:06 -0400

    first actual commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/*
+alert("Prepare to read and get to the end of this incomplete , short website!!!");
+// prompt, used if you want the user input
+alert("Be aware, this is a very average site, that is made by a 13 year old boy that loves coding, please give me some good comments (This is a website in progress, bare with me)");
+alert("There will be future updates coming  (there wont be any spoilers!) again, please bare with me");
+
+var person = prompt("Please enter your name:", "Nicholas J.");
+// pops OK or Cancel to proceed
+var r = confirm("Have fun reading!!!");
